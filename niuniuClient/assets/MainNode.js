75b8426792b22c663af1c95e59a4ff82ba8f23ef
secretdy <351164127@qq.com>
2021-02-21 10:39:17 +0800

    服务器

--- niuniuClient/assets/MainNode.js
+++ niuniuClient/assets/MainNode.js
@@ -1,27 +1,29 @@
-// Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        
-    },
-
-    onLoad () {
-        console.log("hello world");
-    },
-
-    start () {
 
     },
 
-    update (dt) {},
+    onLoad() {
+        let ws = new WebSocket("ws://101.200.209.8:3000");
+        ws.onopen = function (result) {
+            console.log("on open", result);
+            ws.send("hello world");
+        }
+        ws.onmessage = function (result) {
+            console.log("on message", result.data);
+        }
+        ws.onerror = function (result) {
+            console.log("on error", result);
+        }
+    },
+
+    start() {
+
+    },
+
+    update(dt) { },
 });
